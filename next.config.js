f5bb6b1a32eea2bf51c59454c88bad321398333d
Sean Norwood <norwood.sean@gmail.com>
2022-10-08 23:44:43 -0500

    Add eslint

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
-const withNextra = require('nextra')({
-  theme: 'nextra-theme-docs',
-  themeConfig: './theme.config.js',
-  unstable_staticImage: true,
-})
-module.exports = withNextra()
+/* eslint-disable @typescript-eslint/no-var-requires */
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+  reactStrictMode: true,
+  basePath: "/marmot-docs",
+};
+
+const withNextra = require("nextra")({
+  theme: "nextra-theme-docs",
+  themeConfig: "./theme.config.js",
+  // unstable_staticImage: true,
+});
+module.exports = withNextra(nextConfig);
